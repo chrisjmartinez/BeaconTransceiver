66bd46f0823f9cca49e4eaf6de04e4518e9c7792
Graham Savage <graham.savage@centricconsulting.com>
2013-12-06 09:29:09 -0500

    Adding Beacon sim and guest app skeletons

---
+++ Grand Lucayan - Beacon Sim/Beacon Sim/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Beacon Sim
+//
+//  Created by Graham Savage on 12/6/13.
+//  Copyright (c) 2013 Grand Lucayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Grand Lucayan - Beacon Sim/Beacon Sim/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Beacon Sim
+//
+//  Created by Graham Savage on 12/6/13.
+//  Copyright (c) 2013 Grand Lucayan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
