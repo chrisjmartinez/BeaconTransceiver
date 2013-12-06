796bb6e9d7c088cdb26879b4671894505d1191dd
Graham Savage <graham.savage@centricconsulting.com>
2013-12-06 12:01:40 -0500

    Lobby started

---
+++ Grand Lucayan - App/Grand Lucayan - Guest App/LobbyViewController.h
@@ -0,0 +1,13 @@
+//
+//  LobbyViewController.h
+//  Grand Lucayan - Guest App
+//
+//  Created by Graham Savage on 12/6/13.
+//  Copyright (c) 2013 Grand Lucayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LobbyViewController : UIViewController
+
+@end
---
+++ Grand Lucayan - App/Grand Lucayan - Guest App/LobbyViewController.m
@@ -0,0 +1,23 @@
+//
+//  LobbyViewController.m
+//  Grand Lucayan - Guest App
+//
+//  Created by Graham Savage on 12/6/13.
+//  Copyright (c) 2013 Grand Lucayan. All rights reserved.
+//
+
+#import "LobbyViewController.h"
+
+@interface LobbyViewController ()
+@property   (nonatomic, retain)     IBOutlet    UIImageView     *weather;
+@end
+
+@implementation LobbyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
