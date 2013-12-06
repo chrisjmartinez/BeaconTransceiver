7f53bdaac3e907749363baf958f3b957651eef7f
Graham Savage <graham.savage@centricconsulting.com>
2013-12-06 09:35:51 -0500

    Adding skeleton apps

---
+++ Grand Lucayan - Beacon Sim/Beacon SimTests/Beacon_SimTests.m
@@ -0,0 +1,34 @@
+//
+//  Beacon_SimTests.m
+//  Beacon SimTests
+//
+//  Created by Graham Savage on 12/6/13.
+//  Copyright (c) 2013 Grand Lucayan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Beacon_SimTests : XCTestCase
+
+@end
+
+@implementation Beacon_SimTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
