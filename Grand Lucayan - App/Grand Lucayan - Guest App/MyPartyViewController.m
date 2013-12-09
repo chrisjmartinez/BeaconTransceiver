817fbe58803bde2775636f857140760c54ce0e00
Graham Savage <graham.savage@centricconsulting.com>
2013-12-09 09:42:01 -0500

    -

--- Grand Lucayan - App/Grand Lucayan - Guest App/MyPartyViewController.m
+++ Grand Lucayan - App/Grand Lucayan - Guest App/MyPartyViewController.m
@@ -13,8 +13,10 @@
 #import "CoreNetworkCommunicationResponse.h"
 
 @interface MyPartyViewController () <BaseWebserviceDelegate>
-@property   (nonatomic, assign)     BOOL                isShowing;
-@property   (nonatomic, retain)     GuestWebservice     *guestWS;
+@property   (nonatomic, assign)     BOOL                            isShowing;
+@property   (nonatomic, retain)     GuestWebservice                 *guestWS;
+@property   (nonatomic, retain)     IBOutlet            UITableView *table;
+@property   (nonatomic, retain)     NSArray                         *guests;
 @end
 
 @implementation MyPartyViewController
@@ -28,10 +30,15 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.isShowing = YES;
+    [self.guestWS getGuests];
+    /*
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         while (self.isShowing) {
+            [self.guestWS getGuests];
+            [NSThread sleepForTimeInterval:2];
         }
     });
+     */
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -42,21 +49,35 @@
 #pragma mark - Service methods
 
 - (void)serviceCallDidFinishLoading:(BaseWebservice *)service withResponse:(CoreNetworkCommunicationResponse *)response {
+    if (self.isShowing) {
+        NSError     *error = nil;
+        self.guests = [service array];
+        if (!error) {
+            // Update the display with new data
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.table reloadData];
+            });
+            // Fire another request after a second pause
+            [NSThread sleepForTimeInterval:1];
+            [self.guestWS getGuests];
+        }
+    }
 }
 
 - (void)serviceCallDidFailWithError:(BaseWebservice *)service withError:(NSError *)error {
+    NSLog(@"Error from getGuests %@", error.localizedDescription);
 }
 
 #pragma mark - Table methods
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 6;
+    return self.guests.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *CellIdentifier = @"Cell";
-    
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
 	return cell;
 }
 
